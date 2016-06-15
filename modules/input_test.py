dbeec0e6fcc87c0ecd6551f0b7398c189e2bc8ca
Adam Borowski <aborowski@atlassian.com>
2016-06-15 10:58:42 +0200

    better tester

--- modules/input_test.py
+++ modules/input_test.py
@@ -21,5 +21,5 @@ def test(model):
         truncated = sequence.pad_sequences([indexes], maxlen=80)
 
         result = model.predict_classes(np.array(truncated))
-        print("result:" + "Positive" if result[0] == 1 else "Negative")
-
+        print("input: " + indexes)
+        print("result: " + "Positive" if result[0] == 1 else "Negative")
